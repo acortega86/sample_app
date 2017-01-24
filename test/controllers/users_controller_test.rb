26a565e518f704c7cc9c91d77823a98622355963
Tega <aaron@arrayschool.com>
2017-01-24 16:44:09 -0700

    Add user pagination and index.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,17 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+      @user = users(:aaron)
+      @other_user = users(:mike)
+  end
+
+  test "should redirect from index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,18 +18,50 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     end
 
     test "succesful edit" do
+      # Logging in as a valid user.
       log_in_as(@user)
+      # Sends user to edit user page.
       get edit_user_path(@user)
+      # Are we actially on the user edit page?
       assert_template 'users/edit'
+      # Local variables to be used later.
       name = "Foo Bar"
       email = "foo@bar.com"
+      # Change user's name and email, then hit submit.
       patch user_path(@user), params: { user: {name: name,
                                                email: email,
                                                password: "",
                                                password_confirmation: ""} }
-
+      # Is there a flash?
       assert_not flash.empty?
+      # Send back to user profile page.
       assert_redirected_to @user
+      # Reload user data from database.
+      @user.reload
+      assert_equal name, @user.name
+      assert_equal email, @user.email
+    end
+
+    test "succesful edit with friendly forwarding" do
+      # Sends user to edit user page.
+      get edit_user_path(@user)
+      # Logging in as a valid user.
+      log_in_as(@user)
+      # Are we actially on the user edit page?
+      assert_redirected_to edit_user_url(@user)
+      # Local variables to be used later.
+      name = "Foo Bar"
+      email = "foo@bar.com"
+      # Change user's name and email, then hit submit.
+      patch user_path(@user), params: { user: {name: name,
+                                               email: email,
+                                               password: "",
+                                               password_confirmation: ""} }
+      # Is there a flash?
+      assert_not flash.empty?
+      # Send back to user profile page.
+      assert_redirected_to @user
+      # Reload user data from database.
       @user.reload
       assert_equal name, @user.name
       assert_equal email, @user.email
